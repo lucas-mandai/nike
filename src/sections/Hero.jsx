023a4f3fe19e9ce1a534ca70f55799078ed03286
lucas-mandai <lucas.mandai@lmandai.com>
2023-09-13 16:50:41 -0300

    First Commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <section 
+        id="home"
+        className="w-full flex xl:flex-row flex-col justify-center min-h-screen border-2 gap-10 max-container"
+    >
+        <div className="relative ">
+
+        </div>
+        
+    </section>
+  )
+}
+
+export default Hero
